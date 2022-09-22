0a836689e6a1c786537de2f5c7868034861c3dd1
Prashanthkumar18 <prashanthkumar.xworkz@gmail.com>
2022-09-22 12:25:11 +0530

    commiting method

---
+++ Library.java
@@ -0,0 +1,8 @@
+class Library {
+public static void main (String dev [])  {
+System.out.println("A huge NUmber of Books Stored together");
+}
+
+}
+
+
---
+++ Users.java
@@ -0,0 +1,11 @@
+class Users {
+static String cards [] = {"Platinum cerdit cards", "ICIC bank cerdit cards", "Kotak Urbane cards"};
+public static void main (String args []   ){
+for ( int a=0; a< cards.length ; a++){
+System.out.println ("No of cards are "+ a + "  " + cards[a]  + " " );
+}
+
+
+}
+
+}
