0857d3b8cf5f014c32a5287c49e9ff4c9680b916
Prashanthkumar18 <prashanthkumar.xworkz@gmail.com>
2022-09-22 13:14:17 +0530

    adding variables

---
+++ BeanBag.java
@@ -0,0 +1,15 @@
+class BeanBag{
+ public static void main(String pra[]){
+ String brand = "Flipkart";
+ String colour = "Black";
+ String material = "Leather";
+ String pattern = "XXl Bean Bag";
+ int price = 1599;
+ System.out.println(" Bean Bag brand is " + brand);
+ System.out.println("Bean Bag colour is " + colour);
+ System.out.println("Bean Bag material is " + material);
+ System.out.println("Bean Bag pattern is " + pattern);
+ System.out.println("Bean Bag price is " + price);
+}
+}
+ 
