c0facab4af4709a98fc65a1fb82fb7100ed12332
Prashanthkumar18 <prashanthkumar.xworkz@gmail.com>
2022-09-23 13:11:46 +0530

    adding initalization

---
+++ TeaStallTester.java
@@ -0,0 +1,17 @@
+class TeaStallTester {
+public static void main (String [] args){
+TeaStall.setName ("MBA Chai Wala");
+TeaStall.setContactNo (8656555555l);
+TeaStall.setAddres ("Basvanguadi");
+
+TeaStall.setTeaStallId (54);
+System.out.println ("The Tea Stall Id is : " + TeaStall.getTeaStallId () + " \n" + "The tea stall name is :" + TeaStall.getName () + " \n " + "The tea stall contac no is :" + TeaStall.getContactNo () + " \n " + "The tea stall is located in :" + TeaStall.getAddres ());
+
+
+
+
+
+
+}
+
+}
